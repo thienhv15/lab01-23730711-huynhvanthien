3dbb10785c5b3c8ca8ffeef3a5ddb427962758c1
Văn Thiên 23720711 <thienhv15@gmail.com>
2024-08-31 12:23:20 +0700

    Update bai20.py

--- lab01-23730711-huynhvanthien/bai20.py
+++ lab01-23730711-huynhvanthien/bai20.py
@@ -8,15 +8,9 @@ Created on Thu Aug 29 13:43:49 2024
 a = float(input("Nhập vào a:"))
 b = float(input("Nhập vào b:"))
 c = float(input("Nhập vào c:"))
-if a > b and b > c:
+if a >= b and a >= c: 
     print('Số lớn nhất là',a)
-if a > c and c > b:
-    print('Số lớn nhất là',a)
-if b > a and a > c:
-    print('Số lớn nhất là',b)
-if b > c and c > a:
-    print('Số lớn nhất là',b)
-if c > a and a > b:
-    print('Số lớn nhất là',c)
-if c > b and b > a:
+if b > a and b >= c:
+    print('Số  nhất là',b)
+if c > a and c > b:
     print('Số lớn nhất là',c)
